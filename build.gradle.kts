c14d0f85919cf61a55bbacdab79f62d25d88302b
FlamingFire <flamingfire0@proton.me>
2026-03-07 19:35:02 +0100

    change version to snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.flamingfire0"
-version = "0.1.0"
+version = "0.1.0-SNAPSHOT"
 description = "A Kotlin library for creating, parsing, and modifying Minecraft Resource Packs."
 
 dependencies {
